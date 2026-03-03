9ae079858a2bc768c936f60eafcce46ac4e56f02
pvasquez <pablo.vasquez@bam.com.gt>
2026-03-02 21:57:31 -0600

    add login widgets

---
+++ lib/features/login/presentation/widgets/primary_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_banking_app/app_colors.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        minimumSize: const Size(double.infinity, 55),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_banking_app/app_colors.dart';
+import 'package:mobile_banking_app/features/login/presentation/views/login2_view.dart';
 import 'package:mobile_banking_app/features/login/presentation/views/login_view.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: AppColors.backgroundColor),
-      home: const LoginView(),
+      home: LoginView2(),
     );
   }
 }
