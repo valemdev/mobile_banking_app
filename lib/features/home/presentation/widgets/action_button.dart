6f58f44ef3d835bb60d95bd680a5f148be19c996
pvasquez <pablo.vasquez@bam.com.gt>
2026-03-08 16:38:19 -0600

    add action buttons

---
+++ lib/features/home/presentation/widgets/action_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const ActionButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Material(
+          color: const Color(0xFFEFEFEF),
+          shape: const CircleBorder(),
+          child: InkWell(
+            onTap: onTap,
+            customBorder: const CircleBorder(),
+            child: SizedBox(
+              width: 56,
+              height: 56,
+              child: Icon(icon),
+            ),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(label),
+      ],
+    );
+  }
+}
