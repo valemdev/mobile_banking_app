5432ccff16a838528b608ce42caf0bbff008af89
pvasquez <pablo.vasquez@bam.com.gt>
2026-03-08 10:33:40 -0600

    add carousel slider package

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Home Page'),
+        const child: []  
       ),
     );
   }
