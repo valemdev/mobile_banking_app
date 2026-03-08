b6e5f8a9a7e38f6b846b26ba908a8a37b92cbd57
pvasquez <pablo.vasquez@bam.com.gt>
2026-03-08 12:53:07 -0600

    add carrousel widget

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:mobile_banking_app/features/home/domain/models/account.dart';
+import 'package:mobile_banking_app/features/home/presentation/widgets/account_card.dart';
+
+final List<Account> accounts = [
+  Account(
+    name: "Account 1",
+    type: "Checking",
+    number: "123456789",
+    currency: "USD",
+    status: "Active",
+    balance: 1000.0,
+  ),
+  Account(
+    name: "Account 2",
+    type: "Savings",
+    number: "987654321",
+    currency: "USD",
+    status: "Active",
+    balance: 2000.0,
+  ),
+  Account(
+    name: "Account 3",
+    type: "Checking",
+    number: "112233445",
+    currency: "USD",
+    status: "Active",
+    balance: 3000.0,
+  ),
+];
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,8 +37,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        const child: []  
-      ),
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 100),
+          CarouselSlider(
+            options: CarouselOptions(height: 120.0),
+            items: accounts.map((account) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: AccountCard(account: account));
+                },
+              );
+            }).toList(),
+          ),
+        ],
+      )),
     );
   }
 }
---
+++ lib/features/home/presentation/widgets/account_card.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_banking_app/features/home/domain/models/account.dart';
+
+class AccountCard extends StatelessWidget {
+  final Account account;
+
+  static const Color primaryColor = Color(0xFF0066FF);
+
+  const AccountCard({
+    super.key,
+    required this.account,
+  });
+
+  IconData _getIcon() {
+    if (account.type == "Checking") {
+      return Icons.account_balance_wallet;
+    } else {
+      return Icons.savings;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(18),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: primaryColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Icon(
+              _getIcon(),
+              color: primaryColor,
+              size: 28,
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  account.name,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  "****${account.number.substring(account.number.length - 4)}",
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  account.type,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "${account.currency} ${account.balance.toStringAsFixed(2)}",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: primaryColor,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                account.status,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: account.status == "Active" ? Colors.green : Colors.red,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
